3328c2a687f7095f6fba3b3752ca847871c144fd
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-01-10 19:10:09 +0530

    import and export

---
+++ import_and_export_1.5/src/App.jsx
@@ -0,0 +1,14 @@
+import  Gallery,{Profile as Pic } from "./components/Gallery";
+import Photo from "./components/Image";
+
+function App() {
+  return (
+    <div>
+      <Gallery />
+      <Pic />
+      <Photo />
+    </div>
+  );
+}
+
+export default App;
---
+++ import_and_export_1.5/src/components/Image.jsx
@@ -0,0 +1,9 @@
+ const Image = () => {
+    return (
+      <img
+        src="https://i.imgur.com/QIrZWGIs.jpg"
+        alt="Alan L. Hart"
+      />
+    );
+  }
+  export default Image;
